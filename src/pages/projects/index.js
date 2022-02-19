6afd906043a0aa3b403375f88ac7fa908bc9397c
Krzysztof Królikowski <>
2022-02-19 21:43:09 +0100

    Add sample pages

---
+++ src/pages/projects/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => (
+  <div>
+    <h1>Test component</h1>
+    <h3>Beauty, courage, and modification. </h3>
+    <p>Lixas manducare! </p>
+  </div>
+)
+
+export default Index
---
+++ src/pages/projects/test.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Test = () => {
+  return (
+    <div>
+      <h1>Test header</h1>
+      <p>Pol, a bene gemna, castus valebat!</p>
+    </div>
+  )
+}
+
+export default Test
